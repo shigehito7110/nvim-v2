fb4b5435e980a145a181432019943c84124861bc
shigehito7110 <shige.7110.330@gmail.com>
2024-04-14 12:49:31 +0900

    初期化

---
+++ lua/internal/settings.lua
@@ -0,0 +1,17 @@
+local opt = vim.opt
+
+-- 全角幅設定
+opt.ambiwidth = 'double'
+
+-- インデント設定
+opt.autoindent = true
+opt.smartindent = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+
+-- カーソル位置表示
+opt.cursorline = true
+opt.relativenumber = true
+opt.termguicolors = true
